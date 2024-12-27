fe7ebcd854e26a927fc6a421b88b9d01e9247b85
Abhirami-KT <abhiramikt@Abhiramis-MacBook-Air.local>
2024-12-27 12:33:52 +0530

    dummy app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App Components</h1>
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
